679e6288efb51547f29066d9a47e0245cb374765
giorgimamardashvili <eanjaparidze75@gmail.com>
2021-11-13 16:14:42 +0400

    bolo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import React from "react";
-import Button1 from "./Button1";
-import Button2 from "./Button2";
 
 function App() {
   return (
-    <div className="App" style={{ display: "flex", paddingTop: "50px" }}>
-      <Button1 />
-      <Button2 />
-    </div>
+    <div className="App" style={{ display: "flex", paddingTop: "50px" }}></div>
   );
 }
 
